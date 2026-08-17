a2e24bad7f7a000689b17ddbc64a2cc5f4f59a61
Lineax17 <lineax17@gmail.com>
2026-08-17 14:29:48 +0200

    improve UI styling and layout

--- src/UI/Models/DownloadItem.cs
+++ src/UI/Models/DownloadItem.cs
@@ -8,8 +8,14 @@ public partial class DownloadItem : ObservableObject
     public Guid Id { get; }
     public string Url { get; }
 
-    [ObservableProperty] private double _progress;
-    [ObservableProperty] private string _status;
+    [ObservableProperty]
+    [NotifyPropertyChangedFor(nameof(StatusColor))]
+    private double _progress;
+
+    [ObservableProperty]
+    [NotifyPropertyChangedFor(nameof(StatusColor))]
+    private string _status;
+
     [ObservableProperty] private string? _errorMessage;
 
     public DownloadItem(Guid id, string url)
@@ -18,4 +24,12 @@ public partial class DownloadItem : ObservableObject
         Url = url;
         _status = "Queued";
     }
+
+    public string StatusColor => Status switch
+    {
+        "Downloading" => "#4CC2FF",
+        "Done" => "#4CAF50",
+        "Error" => "#F44336",
+        _ => "#A0A0A0"
+    };
 }
